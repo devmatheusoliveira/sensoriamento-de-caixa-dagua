3d781a0113ade8d885e055f8f2f9a8ffe73a2a12
marialicas <eialicag@gmail.com>
2026-07-07 14:50:11 -0300

    funcao media e volume

--- main.c
+++ main.c
@@ -9,41 +9,56 @@ void ini_Timer1_Captura_Echo(void);
 
 volatile unsigned int t_subida = 0;
 volatile unsigned int largura = 0;
-volatile unsigned int distancia = 0;
+float distancia = 0;
+volatile unsigned int indexAtual = 0;
+float volumeAtual = 0;
+float media = 0;
+float raio = 28.5;         // mm
+float alturaTotal = 200.0; // mm ou 20cm
+float volumeTotal;         // em ml
+const unsigned int qtdeAmostras = 32;
+unsigned int amostrasDistancia[qtdeAmostras];
 
 void main(void)
 {
+    volumeTotal = 3.14 * raio * raio * alturaTotal;
     ini_uCon();
     ini_P1_P2();
     ini_Timer0_PWM_Trigger();
     ini_Timer1_Captura_Echo();
 
-    do {
+    do
+    {
     } while (1);
 }
 
 #pragma vector = TIMER1_A1_VECTOR
 __interrupt void RTI_Captura_Echo(void)
 {
-    switch (TA1IV) {
+    switch (TA1IV)
+    {
 
     case TA1IV_TACCR1:
 
-        if (TA1CCTL1 & COV) {
+        if (TA1CCTL1 & COV)
+        {
             TA1CCTL1 &= ~COV;
         }
 
-        if (TA1CCTL1 & CCI) {
+        if (TA1CCTL1 & CCI)
+        {
             t_subida = TA1CCR1;
-        } else {
-            largura = TA1CCR1 - t_subida;
-            distancia = largura / 58;
-
-            if (distancia < DIST_LIMIAR) {
-                P1OUT |= BIT0;
-            } else {
-                P1OUT &= ~BIT0;
+        }
+        else
+        {
+            if (indexAtual > qtdeAmostras)
+            {
+                indexAtual = 0;
             }
+            largura = TA1CCR1 - t_subida;
+            distancia = largura / 5.8; // para dar a distancia em mm
+            amostrasDistancia[indexAtual] = distancia;
+            indexAtual++;
         }
         break;
 
@@ -54,16 +69,16 @@ __interrupt void RTI_Captura_Echo(void)
 
 void ini_Timer0_PWM_Trigger(void)
 {
-    TA0CTL   = TASSEL1 + MC0;
+    TA0CTL = TASSEL1 + MC0;
     TA0CCTL0 = 0;
     TA0CCTL1 = OUTMOD2 + OUTMOD1 + OUTMOD0;
-    TA0CCR0  = 59999;
-    TA0CCR1  = 15;
+    TA0CCR0 = 59999;
+    TA0CCR1 = 15;
 }
 
 void ini_Timer1_Captura_Echo(void)
 {
-    TA1CTL   = TASSEL1 + MC1 + TACLR;
+    TA1CTL = TASSEL1 + MC1 + TACLR;
     TA1CCTL1 = CM1 + CM0 + SCS + CAP + CCIE;
 }
 
@@ -72,14 +87,14 @@ void ini_P1_P2(void)
     P1DIR = 0xFF;
     P1OUT = 0;
 
-    P1SEL  |= BIT2;
+    P1SEL |= BIT2;
     P1SEL2 &= ~BIT2;
 
-    P2DIR  = 0xFF;
+    P2DIR = 0xFF;
     P2DIR &= ~BIT1;
-    P2OUT  = 0;
+    P2OUT = 0;
 
-    P2SEL  |= BIT1;
+    P2SEL |= BIT1;
     P2SEL2 &= ~BIT1;
 }
 
@@ -87,9 +102,34 @@ void ini_uCon(void)
 {
     WDTCTL = WDTPW | WDTHOLD;
 
-    DCOCTL  = CALDCO_1MHZ;
+    DCOCTL = CALDCO_1MHZ;
     BCSCTL1 = CALBC1_1MHZ;
     BCSCTL2 = 0;
 
     __enable_interrupt();
+}
+
+float calcularMedia()
+{
+    int soma = 0;
+    media = 0;
+
+    for (int i = 0; i < qtdeAmostras; i++)
+    {
+        soma = +amostrasDistancia[i];
+    }
+
+    media = soma / qtdeAmostras;
+
+    return media; // em mm
+}
+
+float calcularVolume()
+{
+    calcularMedia();
+    float alturaAtual = 0;
+    alturaAtual = alturaTotal - media;
+    volumeAtual = (3.14 * raio * raio * alturaAtual) / 1000;
+
+    return volumeAtual; // em ml
 }
